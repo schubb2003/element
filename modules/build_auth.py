14d79297b2dd6908e0c84ba533d507acd44fe054
Scott Chubb <schubb@gmail.com>
2021-03-16 08:08:05 -0400

    properly handle hostname without lookup entry

--- modules/build_auth.py
+++ modules/build_auth.py
@@ -8,6 +8,8 @@
 
 import base64
 import json
+import socket
+import sys
 import requests
 from modules.connect_cluster import connect_cluster_rest as connect_cluster
 
@@ -16,55 +18,61 @@ def build_auth(mvip, user, user_pass, mvip_node=None):
     """
     Authorization for connecting to the cluster mvip on port 443
     """
-    if mvip_node is None:
-        mvip_node = "cluster"
-    token_url = "https://" + mvip + "/auth/connect/token"
-    m_url = "https://" + mvip + "/json-rpc/"
-    n_url = "https://" + mvip + ":442/json-rpc/"
+    try:
+        socket.gethostbyname(mvip)
+        if mvip_node is None:
+            mvip_node = "cluster"
+        token_url = "https://" + mvip + "/auth/connect/token"
+        m_url = "https://" + mvip + "/json-rpc/"
+        n_url = "https://" + mvip + ":442/json-rpc/"
 
-    files = {
-        'client_id': (None, 'element-automation'),
-        'grant_type': (None, 'password'),
-        'username': (None, user),
-        'password': (None, user_pass),
-    }
+        files = {
+            'client_id': (None, 'element-automation'),
+            'grant_type': (None, 'password'),
+            'username': (None, user),
+            'password': (None, user_pass),
+        }
 
-    response = requests.post(url=token_url, files=files, verify=False)
-    res_code = response.status_code
-    if res_code is 200:
-        print("Using token authentication")
-        token_json = json.loads(response.text)
-        token_out = token_json['access_token']
-        auth_bear = "Bearer " + token_out
-        headers = {'Authorization': auth_bear}
+        response = requests.post(url=token_url, files=files, verify=False)
+        res_code = response.status_code
+        if res_code is 200:
+            print("Using token authentication")
+            token_json = json.loads(response.text)
+            token_out = token_json['access_token']
+            auth_bear = "Bearer " + token_out
+            headers = {'Authorization': auth_bear}
 
-    else:
-        print("Using basic auth without token grant")
-        auth = (user + ":" + user_pass)
-        encode_key = base64.b64encode(auth.encode('utf-8'))
-        basic_auth = bytes.decode(encode_key)
+        else:
+            print("Using basic auth without token grant")
+            auth = (user + ":" + user_pass)
+            encode_key = base64.b64encode(auth.encode('utf-8'))
+            basic_auth = bytes.decode(encode_key)
 
-        headers = {
-            'Content-Type': "application/json",
-            'Authorization': "Basic %s" % basic_auth,
-            'Cache-Control': "no-cache",
-            }
+            headers = {
+                'Content-Type': "application/json",
+                'Authorization': "Basic %s" % basic_auth,
+                'Cache-Control': "no-cache",
+                }
 
-    if mvip_node == "cluster":
-        url = m_url + "9.0"
-    else:
-        url = n_url + "9.0"
+        if mvip_node == "cluster":
+            url = m_url + "9.0"
+        else:
+            url = n_url + "9.0"
 
-    payload = build_payload()
-    response_json = connect_cluster(headers, url, payload)
-    # Return the latest API supported and provide that output to
-    #   connect_cluster module
-    latest_api = get_version(response_json)
-    if mvip_node == "cluster":
-        url = m_url + latest_api
-    else:
-        url = n_url + latest_api
-    return headers, url
+        payload = build_payload()
+        response_json = connect_cluster(headers, url, payload)
+        # Return the latest API supported and provide that output to
+        #   connect_cluster module
+        latest_api = get_version(response_json)
+        if mvip_node == "cluster":
+            url = m_url + latest_api
+        else:
+            url = n_url + latest_api
+        return headers, url
+    except Exception as my_except:
+        print(f"Unable to lookup {mvip}, please verify name/dns/etc, "
+              f"script will exit")
+        sys.exit(1)
 
 
 def build_payload():
